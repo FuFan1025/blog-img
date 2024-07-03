12aafc5b4d348505a8eb5e73d4f8f6f5afb80998
FuFan1025 <135838897+FuFan1025@users.noreply.github.com>
2024-07-03 14:24:38 +0800

    Update api.php

--- api.php
+++ api.php
@@ -1,10 +1,28 @@
 <?php
-//初始化随机数生成器种子,这行代码也可以删除
-$seed = time();
-//获取随机数
-$num = rand(1,18);
-//拼接图片地址
-$picpath = 'https://images.weserv.nl/?url=https://gitee.com/fufan1025/blog_img/raw/master/'+img;
-//重定位到图片
-die(header("Location: $picpath"));
+//此php和保存链接的txt文件放在同一目录下
+$filename = "img.txt";  /*保存链接的txt文件名*/
+if(!file_exists($filename)){
+    die('文件不存在');
+}
+//从文本获取链接
+$pics = [];
+$fs = fopen($filename, "r");
+while(!feof($fs)){
+    $line=trim(fgets($fs));
+    if($line!=''){
+        array_push($pics, $line);
+    }
+}
+//从数组随机获取链接
+$pic = $pics[array_rand($pics)];
+//返回指定格式
+$type=$_GET['type'];
+switch($type){
+//JSON返回
+case 'json':
+    header('Content-type:text/json');
+    die(json_encode(['pic'=>$pic]));
+default:
+    die(header("Location: $pic"));
+}
 ?>
